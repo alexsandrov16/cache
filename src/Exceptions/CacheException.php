4d70768ccddba79a5137ced8f0886594e7d3fbd6
alexsandrov16 <al3x5.dev@gmail.com>
2024-08-19 16:42:27 -0400

    Initial commit

---
+++ src/Exceptions/CacheException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mk4U\Cache\Exceptions;
+
+use Psr\SimpleCache\CacheException as SimpleCacheException;
+
+/**
+ * CacheException class
+ */
+class CacheException extends \Exception implements SimpleCacheException
+{
+    
+}
